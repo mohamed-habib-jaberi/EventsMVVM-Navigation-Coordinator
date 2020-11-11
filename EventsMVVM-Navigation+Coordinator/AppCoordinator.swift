0f58f99b25d54511a72eae803009db7633e23277
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-11-11 16:08:58 +0100

    Create AppCoordinator

---
+++ EventsMVVM-Navigation+Coordinator/AppCoordinator.swift
@@ -0,0 +1,33 @@
+//
+//  AppCoordinator.swift
+//  EventsMVVM-Navigation+Coordinator
+//
+//  Created by mohamed  habib on 11/11/2020.
+//  Copyright © 2020 mohamed  habib. All rights reserved.
+//
+
+import UIKit
+
+protocol Coordinator {
+    
+    var childCoordinators: [Coordinator] {get}
+    func start()
+    
+}
+final class AppCoordinator: Coordinator {
+    private(set) var childCoordinators: [Coordinator] = []
+    private let window: UIWindow
+    
+    init(window: UIWindow) {
+        self.window = window
+    }
+    
+    func start() {
+        let navigationController = UINavigationController()
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+    
+    
+    
+}
