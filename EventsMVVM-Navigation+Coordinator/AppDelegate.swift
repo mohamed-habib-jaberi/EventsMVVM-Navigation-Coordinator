99b04119f786f4e17d88fb61e5d4950bfbda8fa9
Mohamed JABERI <mohamed.jaberi@externe.e-i.com>
2021-04-08 14:13:34 +0200

    add path for core data database location

--- EventsMVVM-Navigation+Coordinator/AppDelegate.swift
+++ EventsMVVM-Navigation+Coordinator/AppDelegate.swift
@@ -13,6 +13,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // path for location of sqlite database
+        let paths = NSSearchPathForDirectoriesInDomains(FileManager.SearchPathDirectory.documentDirectory, FileManager.SearchPathDomainMask.userDomainMask, true)
+        print(paths[0])
         return true
     }
 
